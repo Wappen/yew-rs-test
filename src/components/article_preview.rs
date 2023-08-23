f92422e3947cbe210f013a631a1df36a01af333e
lkoxholt <lenz.koxholt@cho-time.de>
2023-08-23 12:31:04 +0200

    Remove length limit for article description

--- src/components/article_preview.rs
+++ src/components/article_preview.rs
@@ -11,22 +11,13 @@ pub struct Props {
 
 #[function_component(ArticlePreview)]
 pub fn article_preview(props: &Props) -> Html {
-    let description = if props.article.description.len() > 100 {
-        format!(
-            "{}…",
-            &props.article.description[0..props.article.description.len().min(100)].trim()
-        )
-    } else {
-        props.article.description.clone()
-    };
-
     html! {
         <Card<MainRoute>
                 title={ props.article.title.clone() }
                 subtitle={ props.article.subtitle() }
                 thumbnail={ props.article.thumbnail.clone() }
                 to={ MainRoute::Article { id: props.article.id } }>
-            { description }
+            { props.article.description.clone() }
         </Card<MainRoute>>
     }
 }
